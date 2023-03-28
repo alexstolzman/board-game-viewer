915f5b09fb6b27001ed6177bbca776a2e99ecf13
alexstolzman <stolzmanalex@gmail.com>
2023-03-28 12:32:14 -0500

    Added sass and some styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Stats from './components/Stats'
 import Header from './components/Header'
 import Upload from './components/Upload'
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,9 @@ import { NavLink } from 'react-router-dom'
 
 export default function NavBar(){
     return(
-        <div>
-            <flex-box>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="upload">Upload</NavLink>
-            </flex-box>
+        <div className='navBar'>
+                <NavLink to="/" className="navItem">Home</NavLink>
+                <NavLink to="upload" className="navItem">Upload</NavLink>
         </div>
     )
 }
