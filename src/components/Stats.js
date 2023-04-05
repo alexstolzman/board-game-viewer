81d64d52e32549cfbfc73926c9a614103c5d8657
alexstolzman <stolzmanalex@gmail.com>
2023-04-05 15:34:11 -0500

    Changed state and how I store the data

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,36 +1,57 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
+import { PieChart } from 'react-minimal-pie-chart';
+
 export default function Stats(){
     const {state} = useLocation();
     const {file}=state
-    const [gameData, setGameData]=useState(new Map())
-    let games=new Map()
+
+    const [games, setGames]=useState({
+      names: [],
+      plays: [],
+      playTime: []
+    })
+
+    //let games=new Map()
 
     useEffect(()=>{
       const data=JSON.parse(file)
       
       for(let obj in data){
+          if(games.names.includes(data[obj].Game)){
+            //sconsole.log(games.names)
+          }
+          else{
+            setGames(games=>({...games, names : ["...games.names, data[obj].Game"]}))
+            console.log(games)
+          }
+
         //Set number of plays per game
-        if(games.has(data[obj].Game)){
-          games.set(data[obj].Game, games.get(data[obj].Game)+1)
-        }
-        else{
-          games.set(data[obj].Game, 0)
-        }
-        games.delete(undefined)
+        // if(games.has(data[obj].Game)){
+        //   games.set(data[obj].Game, games.get(data[obj].Game)+1)
+        // }
+        // else{
+        //   games.set(data[obj].Game, 0)
+        // }
+        // games.delete(undefined)
 
       }
 
-      console.log(games)
+      //console.log(games)
       
     }, [])
 
     return(
        <div>
          <h1>Stats</h1>
+
           <label>
           Games
+
+
+
+     
 
           </label>
           <label>
@@ -39,7 +60,7 @@ export default function Stats(){
           <label>
             Time Played(Pie chart with total number of hours above)
           </label>
-        <p>{file}</p>
+        <p></p>
        </div>
         
     )
