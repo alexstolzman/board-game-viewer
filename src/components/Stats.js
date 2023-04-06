907ba60dcb0b0fd7b3d56db71153ada103224b13
alexstolzman <stolzmanalex@gmail.com>
2023-04-06 09:56:48 -0500

    Added pie chart data

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-import { PieChart } from 'react-minimal-pie-chart';
+import { Chart } from "react-google-charts";
 
 export default function Stats(){
     const {state} = useLocation();
@@ -13,19 +13,30 @@ export default function Stats(){
       playTime: []
     })
 
-    //let games=new Map()
+    const [playDataPie, setPlayDataPie]=useState([])
+    const [timeDataPie, setTimeDataPie]=useState([])
+
 
     useEffect(()=>{
       const data=JSON.parse(file)
       
       for(let obj in data){
           if(games.names.includes(data[obj].Game)){
-            //sconsole.log(games.names)
+            const newValues={...games}
+            const index=games.names.indexOf(data[obj].Game)
+            newValues.plays[index]++
+            newValues.playTime[index]=parseInt(newValues.playTime[index])+parseInt(data[obj].Time)
+            setGames(newValues)
+
           }
-          else{
-            setGames(games=>({...games, names : ["...games.names, data[obj].Game"]}))
-            console.log(games)
+          else if(data[obj].Game!=undefined){
+            const newValues={...games}
+            newValues.names.push(data[obj].Game)
+            newValues.plays.push(1)
+            newValues.playTime.push(parseInt(data[obj].Time))
+            setGames(newValues)
           }
+
 
         //Set number of plays per game
         // if(games.has(data[obj].Game)){
@@ -39,28 +50,52 @@ export default function Stats(){
       }
 
       //console.log(games)
+      var pieChart=[]
+      var time=[]
+      pieChart.push(["Game", "Number of Plays"])
+      time.push(["Game","Time Played"])
+          for(let i=0;i<games.names.length;i++){
+            pieChart.push([games.names[i],games.plays[i]])
+            time.push([games.names[i],games.playTime[i]])
+          }
+          setPlayDataPie(pieChart)
+          setTimeDataPie(time)
+
+      //console.log(games)
       
     }, [])
+
+  const options1 = {
+  title: "Number of Plays",
+  is3D: true,
+}
+
+const options2 = {
+  title: "Time Played(Min)",
+  is3D: true,
+}
 
     return(
        <div>
          <h1>Stats</h1>
 
-          <label>
-          Games
+           <Chart
+      chartType="PieChart"
+      data={playDataPie}
+      options={options1}
+      width={"100%"}
+      height={"400px"}
+    />
 
-
+      <Chart
+      chartType="PieChart"
+      data={timeDataPie}
+      options={options2}
+      width={"100%"}
+      height={"400px"}
+    />
 
      
-
-          </label>
-          <label>
-            Total Plays(histogram)
-          </label>
-          <label>
-            Time Played(Pie chart with total number of hours above)
-          </label>
-        <p></p>
        </div>
         
     )
