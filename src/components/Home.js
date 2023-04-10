070a16a19fb534b049696c609041e50a4a29bb65
alexstolzman <stolzmanalex@gmail.com>
2023-04-10 10:26:38 -0500

    Cleaned up some code and added some css to make Home and Upload pages look better

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,8 @@ export default function Home(){
         <div className='Home'>
             <h1>Home</h1>
             <h3>About</h3>
-            <p>This is a simple site created to take a csv file of board game stats and output a visual representation of the stats</p>
-            <p>Currently uses a specific .csv format, so you may have to make slight adjustments for it to work correctly with your file.</p>
+            <p>This site was created to take a .csv file of board game stats and output a visual representation of the important data </p>
+            <p id="homeNote">Note: Slight adjustments will most likely need to be made for this to work with your .csv file(check the upload page for more information)</p>
             <p>An example of the stats page is shown below</p>
             <img src="./stats.png" border="2px black" alt="Example of stats page"/>
         </div>
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 export default function Upload(){
     const [isSelected, setSelected]=useState(false)
@@ -14,31 +14,29 @@ export default function Upload(){
 
     }
 
-
-
     const onSubmission=(e)=>{
         let path = `..//stats`; 
 
         //File validation
         e.preventDefault()
-         var reader = new FileReader();
-         
-          reader.readAsText(selectedFile);
+        var reader = new FileReader(); 
+        reader.readAsText(selectedFile);
 
         reader.onload = function() {
-           // console.log(reader.result);
             var csv=reader.result
             var lines=csv.split("\n")
             var result=[]
             var headers=lines[0].split(",")
-              for(var i=1;i<lines.length;i++){
-            var obj = {};
-            var currentline=lines[i].split(",");
-            for(var j=0;j<headers.length;j++){
-                obj[headers[j]] = currentline[j];
-            }
-            result.push(obj);
+
+            for(var i=1;i<lines.length;i++){
+                var obj = {};
+                var currentline=lines[i].split(",");
+                for(var j=0;j<headers.length;j++){
+                    obj[headers[j]] = currentline[j];
+                }
+                result.push(obj);
             }  
+            
             //return result; //JavaScript object
             result= JSON.stringify(result); //JSON
 
@@ -53,26 +51,32 @@ export default function Upload(){
 
     return(
         <div className="upload">
+        <div className="uploadDesc">
             <h1>Upload</h1>
-            <p>Upload .csv file with the following attributes: Game, Time, and Outcome. Time should be in minutes and Outcome should have "Won" or "Lost"</p>
+            <p>Upload a .csv file that follows the rules below</p>
+            <ul>
+                <li>Must have the columns <b>Game</b>,<b> Time</b>, and <b>Outcome</b></li>
+                <li>Time column should be in minutes</li>
+                <li>Outcome column should have <b>'Won'</b>,<b> 'Lost'</b>, or <b>'N/A'</b></li>
+            </ul>
+        </div>
             <form onSubmit={onSubmission}>
                 <input type="file" accept=".csv, text/csv" onChange={onFileChange}></input>
-            {isSelected ? (
-				<div>
-					<p>Filename: {selectedFile.name}</p>
-					<p>Filetype: {selectedFile.type}</p>
-					<p>Size in bytes: {selectedFile.size}</p>
-					<p>
-						lastModifiedDate:{' '}
-						{selectedFile.lastModifiedDate.toLocaleDateString()}
-					</p>
-				</div>
-			) : (
-				<p>Select a file to show details</p>
-			)}
-            <button className="submitBtn" type="submit" >Submit</button>
+                {isSelected ? (
+                    <div>
+                        <p>Filename: {selectedFile.name}</p>
+                        <p>Filetype: {selectedFile.type}</p>
+                        <p>Size in bytes: {selectedFile.size}</p>
+                        <p>
+                            lastModifiedDate:{' '}
+                            {selectedFile.lastModifiedDate.toLocaleDateString()}
+                        </p>
+                    </div>
+                ) : (
+                    <p>Select a file to show details</p>
+                )}
+                <button className="submitBtn" type="submit" >Submit</button>
             </form>
-        
         </div>
     )
 }
